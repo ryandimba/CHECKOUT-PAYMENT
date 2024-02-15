e4aeaed1b22c0fb916122d04a7ffd0b4da77eb62
EVERLYNE AKINYI <evahaudi2354@gmail.com>
2024-02-15 21:05:05 +0300

    initialized Django projct

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'barber_web',
+        'USER': 'root',
+        'PASSWORD': '@2023Germany',
+        'HOST': 'localhost',  # Or the IP address of your MySQL server
+        'PORT': '3306',  # MySQL default port
     }
 }
+
 
 
 # Password validation
